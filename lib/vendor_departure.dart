50d7361d848acd209625cc4cefb81d1536369b21
LanxiangWang <lanxiang.wang13@gmail.com>
2019-03-01 21:55:53 -0500

    add floating button for vendor main page

--- lib/vendor_departure.dart
+++ lib/vendor_departure.dart
@@ -11,10 +11,40 @@ class _VendorDepartureState extends State<VendorDeparture> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("hello");
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: Text("Today' Plan"),
       ),
-      body: Text('placeholder'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                  // padding: EdgeInsets.only(right: 12.0),
+                  margin: EdgeInsets.symmetric(),
+                  width: 200,
+                  decoration: new BoxDecoration(
+                    border: new Border(
+                      bottom: new BorderSide(width: 3.0, color: Colors.blue),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Today's Menu",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
